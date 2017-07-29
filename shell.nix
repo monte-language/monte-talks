9a3c4ae3953c36fcc2dbebac95e40d38b3cbd424
Corbin <cds@corbinsimpson.com>
2017-07-28 22:40:37 -0700

    Publish some trash.

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+let
+  inherit (nixpkgs) pkgs;
+in pkgs.stdenv.mkDerivation {
+  name = "talks-env";
+  buildInputs = with pkgs; [
+    git
+    texlive.combined.scheme-medium
+    texlive.bin.xdvi
+    lyx firefox
+  ];
+}
