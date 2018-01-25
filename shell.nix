0d667971aed558d00a99b823a7444f5ce9a21dc6
Corbin <cds@corbinsimpson.com>
2018-01-25 07:34:42 -0800

    Let's talk about our nanopass module.

--- shell.nix
+++ shell.nix
@@ -5,9 +5,11 @@ in pkgs.stdenv.mkDerivation {
   name = "talks-env";
   buildInputs = with pkgs; [
     git
-    texlive.combined.scheme-medium
+    unzip
+    texlive.combined.scheme-full
     texlive.bin.xdvi
     lyx hunspellDicts.en-us
-    firefox impressive
+    # firefox
+    impressive
   ];
 }
